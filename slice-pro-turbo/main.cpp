0c513ecb02f20deeccb3153a3a82c2dea4ca6a12
digitalHTTK <57527328+digitalHTTK@users.noreply.github.com>
2020-12-02 22:10:30 +1000

    test

--- slice-pro-turbo/main.cpp
+++ slice-pro-turbo/main.cpp
@@ -1,11 +1,13 @@
 #include "widget.h"
 
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
     w.show();
+    qDebug() << "РУКАВ ВСЕ ЕЩЕ ХУ*";
     return a.exec();
 }
