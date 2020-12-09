57655f442390fa7513c6b083ac774aafdb77bfdc
Alexander <sansanichick@gmail.com>
2020-12-10 01:19:25 +1000

    Camera translation fix

--- slice-pro-turbo/glwidget.cpp
+++ slice-pro-turbo/glwidget.cpp
@@ -135,8 +135,8 @@ void GLWidget::mouseMoveEvent(QMouseEvent *event)
         setXRotation(xRot + 8 * dy);
         setZRotation(zRot + 8 * dx);
     } else if (event->buttons() == Qt::MiddleButton) {
-        xPos += dx / 180.0;
-        yPos -= dy / 180.0;
+        xPos += dx / ((double)width() / 3.0);
+        yPos -= dy / ((double)height() / 2.0);
         updateGL();
     }
 
