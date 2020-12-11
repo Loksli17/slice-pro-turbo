9a2fd96385de59c3ff14b86f2d8a1d28b9736ed3
Alexander <sansanichick@gmail.com>
2020-12-11 20:13:50 +1000

    haha

---
+++ build-slice-pro-turbo-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/ui_widget.h
@@ -0,0 +1,66 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QGridLayout *gridLayout;
+    QLabel *label;
+    QPushButton *pushButton;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName(QString::fromUtf8("Widget"));
+        Widget->resize(800, 600);
+        gridLayout = new QGridLayout(Widget);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        label = new QLabel(Widget);
+        label->setObjectName(QString::fromUtf8("label"));
+
+        gridLayout->addWidget(label, 0, 0, 1, 1);
+
+        pushButton = new QPushButton(Widget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+
+        gridLayout->addWidget(pushButton, 1, 0, 1, 1);
+
+
+        retranslateUi(Widget);
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QCoreApplication::translate("Widget", "Widget", nullptr));
+        label->setText(QCoreApplication::translate("Widget", "TextLabel", nullptr));
+        pushButton->setText(QCoreApplication::translate("Widget", "\320\240\320\243\320\232\320\220\320\222 \320\245\320\243*", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
